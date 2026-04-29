8516b4b0dc4309ddcc497398017af90eb828a8b2
VanshajPoonia <vanshajtheunique@gmail.com>
2026-04-29 09:37:59 +0530

    Add console output wrapper

---
+++ kernel/src/console.rs
@@ -0,0 +1,43 @@
+//! Beginner-friendly text output layer for the kernel.
+//!
+//! The console module is the public place for screen printing. The low-level
+//! VGA details stay in `vga_text`, so the rest of the kernel can use familiar
+//! `print!` and `println!` macros.
+
+use core::fmt;
+
+use crate::vga_text;
+use crate::vga_text::color::Color;
+
+pub fn init(foreground: Color, background: Color) {
+    vga_text::init(foreground, background);
+}
+
+pub fn clear() {
+    vga_text::clear();
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    vga_text::print(args);
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::console::_print(format_args!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {
+        $crate::print!("\n")
+    };
+    ($fmt:expr) => {
+        $crate::print!(concat!($fmt, "\n"))
+    };
+    ($fmt:expr, $($arg:tt)*) => {
+        $crate::print!(concat!($fmt, "\n"), $($arg)*)
+    };
+}
