45b91550073ce1b9ac9778aee29d50a2d4c45398
VanshajPoonia <vanshajtheunique@gmail.com>
2026-05-05 21:43:11 +0530

    Attach RTL8139 to custom QEMU run

--- scripts/run-custom.ps1
+++ scripts/run-custom.ps1
@@ -9,4 +9,6 @@ if (-not (Test-Path $image)) {
 
 qemu-system-x86_64 `
     -drive "format=raw,file=$image" `
+    -netdev "user,id=net0,hostfwd=udp::9000-:9000" `
+    -device "rtl8139,netdev=net0,mac=52:54:00:12:34:56" `
     -serial stdio
